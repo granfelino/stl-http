b06a1dad6c06a937e724426aa0d7730377531b85
granfelino <luciusbest@protonmail.com>
2025-11-29 21:02:09 +0100

    [docs] Added __init__ docstring.

--- src/stlhttp/__init__.py
+++ src/stlhttp/__init__.py
@@ -1,2 +1,14 @@
+"""
+This package contains a HTTP server made with http.server
+GET /items
+GET /items/id
+POST /items/id
+
+are handled here.
+
+
+Usage:
+`python -m stlhttp`
+"""
 # empty
 # __init__.py
